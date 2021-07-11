921f17e5ca09761d6d985e4cdcc5247f8dcde70e
Yousef Obeidat <y.linux96@gmail.com>
2021-07-11 16:03:45 +0300

    class-06

---
+++ app.js
@@ -0,0 +1,248 @@
+'use strict'
+
+let Seattle ={
+
+min : 23,
+max :65,
+avgCookie: 6.3,
+
+
+getavg = function(){
+
+    this.avgCookie=random(16,57)+'per hour'
+    console.log(this.avgCookie);
+}
+
+}
+
+Seattle.getavg();
+
+
+
+
+function random(min , max){
+
+    return Math.floor(Math.random() * (max - min + 1))+min;
+    }
+    console.log(random(23,65));
+
+
+
+let parent=document.getElementById('salmon cookies');
+console.log(parent);
+
+let list=document.createElement('list');
+console.log(list);
+
+ parent.appendChild (list);
+
+ let h2element= document.createElement ('h2');
+ h2element.textContent=this.avgCookie
+
+ let ulElement = document.createElement('ul')
+
+
+
+
+
+
+ let Tokyo ={
+
+    min : 3,
+    max :24,
+    avgCookie: 1.2,
+    
+    
+    getavg = function(){
+    
+        this.avgCookie=random(3,24)+'per hour'
+        console.log(this.avgCookie);
+    }
+    
+    }
+    
+    Tokyo.getavg();
+    
+
+
+
+    function random(min , max){
+
+        return Math.floor(Math.random() * (max - min + 1))+min;
+        }
+        console.log(random(3,24));
+    
+    
+    
+    let parent=document.getElementById('salmon cookies');
+    console.log(parent);
+    
+    let list=document.createElement('list');
+    console.log(list);
+    
+     parent.appendChild (list);
+    
+     let h2element= document.createElement ('h2');
+     h2element.textContent=this.avgCookie
+    
+     let ulElement = document.createElement('ul')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    let Dubai ={
+
+        min : 11,
+        max :38,
+        avgCookie: 3.7,
+        
+        
+        getavg = function(){
+        
+            this.avgCookie=random(11,38)+'per hour'
+            console.log(this.avgCookie);
+        }
+        
+        }
+        
+        Dubai.getavg();
+        
+
+        function random(min , max){
+
+            return Math.floor(Math.random() * (max - min + 1))+min;
+            }
+            console.log(random(11,38));
+        
+        
+        
+        let parent=document.getElementById('salmon cookies');
+        console.log(parent);
+        
+        let list=document.createElement('list');
+        console.log(list);
+        
+         parent.appendChild (list);
+        
+         let h2element= document.createElement ('h2');
+         h2element.textContent=this.avgCookie
+        
+         let ulElement = document.createElement('ul')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        let paris ={
+
+            min : 20,
+            max :38,
+            avgCookie: 2.3,
+            
+            
+            getavg = function(){
+            
+                this.avgCookie=random(20,38)+'per hour'
+                console.log(this.avgCookie);
+            }
+            
+            }
+            
+            paris.getavg();
+
+
+            function random(min , max){
+
+                return Math.floor(Math.random() * (max - min + 1))+min;
+                }
+                console.log(random(20,38));
+            
+            
+            
+            let parent=document.getElementById('salmon cookies');
+            console.log(parent);
+            
+            let list=document.createElement('list');
+            console.log(list);
+            
+             parent.appendChild (list);
+            
+             let h2element= document.createElement ('h2');
+             h2element.textContent=this.avgCookie
+            
+             let ulElement = document.createElement('ul')
+
+            HTMLDataListElement.appendChild(ulElement);
+
+
+
+
+
+
+
+
+
+
+
+            
+        let lima ={
+
+            min : 2,
+            max :16,
+            avgCookie: 4.6,
+            
+            
+            getavg = function(){
+            
+                this.avgCookie=random(2,16)+'per hour'
+                console.log(this.avgCookie);
+            }
+            
+            }
+            
+            lima.getavg();
+
+
+
+            function random(min , max){
+
+                return Math.floor(Math.random() * (max - min + 1))+min;
+                }
+                console.log(random(2,16));
+            
+            
+            
+            let parent=document.getElementById('salmon cookies');
+            console.log(parent);
+            
+            let list=document.createElement('list');
+            console.log(list);
+            
+             parent.appendChild (list);
+            
+             let h2element= document.createElement ('h2');
+             h2element.textContent=this.avgCookie
+            
+             let ulElement = document.createElement('ul')
+
